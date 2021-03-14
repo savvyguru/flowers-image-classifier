9e53f94d104598896f944943bd121e7c1540d634
Richard Pung <31462291+savvyguru@users.noreply.github.com>
2021-03-14 22:41:17 +0800

    Update model_ic.py

--- model_ic.py
+++ model_ic.py
@@ -64,11 +64,11 @@ def validation(model, testloader, criterion, device):
 # Define NN function
 def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, validloader, train_data):
     # Import pre-trained NN model 
-    model = getattr(models, model_name)(pretrained=True)
+    model = getattr(models, model_name)(pretrained=False)
     
     # Freeze parameters that we don't need to re-train 
     for param in model.parameters():
-        param.requires_grad = False
+        param.requires_grad = True
         
     # Make classifier
     n_in = next(model.classifier.modules()).in_features
@@ -93,7 +93,7 @@ def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, 
 
             steps += 1
 
-            optimizer.zero_grad()
+            #optimizer.zero_grad()
 
             output = model.forward(images)
             loss = criterion(output, labels)
