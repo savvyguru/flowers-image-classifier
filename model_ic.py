8ffeb5fcc8f7d2fa7b8662363c7b2a644e1889f2
Richard Pung <31462291+savvyguru@users.noreply.github.com>
2021-03-14 22:25:21 +0800

    Update model_ic.py

--- model_ic.py
+++ model_ic.py
@@ -63,7 +63,6 @@ def validation(model, testloader, criterion, device):
 
 # Define NN function
 def make_NN(n_hidden, n_epoch, labelsdict, lr, device, model_name, trainloader, validloader, train_data):
-    n_epoch = 5
     # Import pre-trained NN model 
     model = getattr(models, model_name)(pretrained=True)
     
